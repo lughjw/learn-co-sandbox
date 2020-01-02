1aea73ef3d6fb5c434a24c4f633daa6b37407702
Lugh Wilson <lughjw@gmail.com>
2020-01-02 22:33:33 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+# Don't do this:
+puts(1 #+1)
+
+# Perform a constant expression evaluation
+3
+
+# Assign constant 3 to bare-word variable triangle_sides
+`triangle_sides`
+
+# From the Three Dog Night song:
